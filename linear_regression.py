188d3181ca28ad1d636f201fcc3889844367beeb
NamanV19 <43335375+NamanV19@users.noreply.github.com>
2020-06-17 23:10:37 +0700

    Linear Regression in class form

--- linear_regression.py
+++ linear_regression.py
@@ -1,101 +1,100 @@
-import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-df = pd.read_csv('raw_data.csv').set_index('date')
-
-# ŷ = β0 + β1 * x1 + β2 * x2 + ... + β(p-n) x(p-n) + Ε
-
-#where:
-
-#ŷ is the predicted outcome variable (dependent variable)
-#xj are the predictor variables (independent variables) for j = 1,2,..., p-1 parameters
-#β0 is the intercept or the value of ŷ when each xj equals zero
-#βj is the change in ŷ based on a one unit change in one of the corresponding xj
-#Ε is a random error term associated with the difference between the predicted ŷi value and the actual yi value
-#That last term in the equation for the Linear Regression is a very important one. The most basic form of building a Linear Regression model relies on an algorithm known as Ordinary Least Squares which finds the combination of βj's values which minimize the Ε term.
-
-df.corr()[['meantempm']].sort_values('meantempm')
-
-predictors = ['meantempm_1',  'meantempm_2',  'meantempm_3',
-              'mintempm_1',   'mintempm_2',   'mintempm_3',
-              'meandewptm_1', 'meandewptm_2', 'meandewptm_3',
-              'maxdewptm_1',  'maxdewptm_2',  'maxdewptm_3',
-              'mindewptm_1',  'mindewptm_2',  'mindewptm_3',
-              'maxtempm_1',   'maxtempm_2',   'maxtempm_3']
-df2 = df[['meantempm'] + predictors]
-
-#%matplotlib inline
-
-# manually set the parameters of the figure to and appropriate size
-plt.rcParams['figure.figsize'] = [16, 22]
-
-# call subplots specifying the grid structure we desire and that
-# the y axes should be shared
-fig, axes = plt.subplots(nrows=6, ncols=3, sharey=True)
-
-# Since it would be nice to loop through the features in to build this plot
-# let us rearrange our data into a 2D array of 6 rows and 3 columns
-arr = np.array(predictors).reshape(6, 3)
-
-# use enumerate to loop over the arr 2D array of rows and columns
-# and create scatter plots of each meantempm vs each feature
-for row, col_arr in enumerate(arr):
-    for col, feature in enumerate(col_arr):
-        axes[row, col].scatter(df2[feature], df2['meantempm'])
-        if col == 0:
-            axes[row, col].set(xlabel=feature, ylabel='meantempm')
-        else:
-            axes[row, col].set(xlabel=feature)
-plt.show()
-
-# import the relevant module
 import statsmodels.api as sm
-
-# separate our my predictor variables (X) from my outcome variable y
-X = df2[predictors]
-y = df2['meantempm']
-
-# Add a constant to the predictor variable set to represent the Bo intercept
-X = sm.add_constant(X)
-X.ix[:5, :5]
-
-# (1) select a significance value
-alpha = 0.05
-
-# (2) Fit the model
-model = sm.OLS(y, X).fit()
-
-# (3) evaluate the coefficients' p-values
-model.summary()
-
-# (3) cont. - Identify the predictor with the greatest p-value and assess if its > our selected alpha.
-#             based off the table it is clear that meandewptm_3 has the greatest p-value and that it is
-#             greater than our alpha of 0.05
-
-# (4) - Use pandas drop function to remove this column from X
-X = X.drop('meandewptm_3', axis=1)
-
-# (5) Fit the model
-model = sm.OLS(y, X).fit()
-
-model.summary()
-
-model = sm.OLS(y, X).fit()
-model.summary()
-
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-# instantiate the regressor class
-regressor = LinearRegression()
-
-# fit the build the model by fitting the regressor to the training data
-regressor.fit(X_train, y_train)
-
-# make a prediction set using the test set
-prediction = regressor.predict(X_test)
-
-# Evaluate the prediction accuracy of the model
 from sklearn.metrics import mean_absolute_error, median_absolute_error
-print("The Explained Variance: %.2f" % regressor.score(X_test, y_test))
-print("The Mean Absolute Error: %.2f degrees celsius" % mean_absolute_error(y_test, prediction))
-print("The Median Absolute Error: %.2f degrees celsius" % median_absolute_error(y_test, prediction))
+
+
+class Linear_Regression():
+    def __init__(self, df, correlation_value):
+        self.predictors = []
+        self.df = df
+        self.correlation_value = correlation_value
+
+    def correlation_assessment(self):
+        self.df.corr()[['meantempm']].sort_values('meantempm')
+
+    def print_df_corr(self):
+        print(self.df.corr())
+
+    def get_suitable_predictors(self):
+        corr_values = self.df.corr()[['meantempm']]
+        corr_values_processed = corr_values.unstack()
+        var_names = []
+        count = 0
+
+        for key, value in corr_values_processed.items():
+            var_names.append(key[1])
+            print(key[1])
+        for key, value in corr_values_processed.items():
+            if value >= 0.6:
+                self.predictors.append(var_names[count])
+            count = count + 1
+        if 'maxtempm' in self.predictors:
+            self.predictors.remove('maxtempm')
+        if 'mintempm' in self.predictors:
+            self.predictors.remove('mintempm')
+        if 'meantempm' in self.predictors:
+            self.predictors.remove('meantempm')
+        df2 = self.df[['meantempm'] + self.predictors]
+
+        if len(self.predictors) > 3:
+            return df2
+        else:
+            return False
+
+    def plot_graph(self, df2):
+        plt.rcParams['figure.figsize'] = [11, 15]
+
+        fig, axes = plt.subplots(nrows=4, ncols=3, sharey=True)
+
+        arr = np.array(self.predictors).reshape(4, 3)
+
+        # use enumerate to loop over the arr 2D array of rows and columns
+        # and create scatter plots of each meantempm vs each feature
+        for row, col_arr in enumerate(arr):
+            for col, feature in enumerate(col_arr):
+                axes[row, col].scatter(df2[feature], df2['meantempm'])
+                if col == 0:
+                    axes[row, col].set(xlabel=feature, ylabel='meantempm')
+                else:
+                    axes[row, col].set(xlabel=feature)
+        plt.show()
+
+    def further_filtering_the_predictors(self, alpha, df2):
+        x = df2[self.predictors]
+        y = df2['meantempm']
+
+        x = sm.add_constant(x)
+        # print("This is x:")
+        # print(x)
+
+        while True:
+            model = sm.OLS(y, x).fit()
+            model_pvalues_list = model.pvalues.tolist()
+            max_value = max(model_pvalues_list)
+
+            if alpha >= max_value:
+                index_of_max_value = model_pvalues_list.index(max_value)
+                variable_removed = self.predictors[index_of_max_value]
+                x = x.drop(variable_removed, axis=1)
+            else:
+                return x
+
+    def print_results(self, x, y):
+        x = x.drop('const', axis=1)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=12)
+
+        regressor = LinearRegression()
+
+        # fit the build the model by fitting the regressor to the training data
+        regressor.fit(x_train, y_train)
+
+        # make a prediction set using the test set
+        prediction = regressor.predict(x_test)
+
+        # Evaluate the prediction accuracy of the model
+        print("The Explained Variance: %.2f" % regressor.score(x_test, y_test))
+        print("The Mean Absolute Error: %.2f degrees celsius" % mean_absolute_error(y_test, prediction))
+        print("The Median Absolute Error: %.2f degrees celsius" % median_absolute_error(y_test, prediction))
